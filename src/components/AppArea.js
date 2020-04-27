9e4853756f626b886454534893914c2f03f71c44
Afshin Mokhtari <afshinator@hotmail.com>
2020-04-26 20:40:42 -0700

    Put some buttons to switch languages for now

--- src/components/AppArea.js
+++ src/components/AppArea.js
@@ -1,18 +1,24 @@
-import React, {useState, useEffect} from "react"
-import styled from "styled-components"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-import Card from "./Card"
+import Card from "./Card";
 
 export const StyledAppArea = styled.main`
   height: 100vh;
-`
+`;
 
 function AppArea(props) {
-  const { t } = useTranslation();
-  const { lastKnownLocation, liveGeoData, userPrefs } = props
-  const [txt, setTxt] = useState('')
-  console.log("AppArea props ", props)
+  const { t, i18n } = useTranslation(["translation", "alert"]);
+  const { lastKnownLocation, liveGeoData, userPrefs } = props;
+  const [txt, setTxt] = useState("");
+  console.log("AppArea props ", props);
+  console.info('language: ', i18n.language)
 
+  const changeLanguage = code => {
+    i18n.changeLanguage(code);
+  };
+
+  /*
   useEffect(()=> {
   // Check to see if we have no idea what their location is:
   // No live geo data, no last location stored, no user-pref object
@@ -23,29 +29,38 @@ function AppArea(props) {
     // so display a message to user if we don't have anything in the error object yet...
     if (liveGeoData.error !== null) {
       // TODO: show something to let user know to enable geolocation...
-      setTxt( t('location no idea', 'no idea') )
+      setTxt( t('alert:geolocation:location no idea', 'no idea') )
       console.info(
         "No idea where you are, Please allow geolocation or enter it manually:",
         liveGeoData
       )
     } else {
-      setTxt(t('waiting for permission', 'wait permis'))
+      setTxt(t('alert:geolocation:waiting for permission', 'wait permis'))
       console.info("waiting for permissions?")
     }
   } else {
-    setTxt( t('we have geo data') )
+    setTxt( t('alert:geolocation:we have geo data') )
     console.info("NOW WE HAVE SOME INFO? ", liveGeoData)
   }
   }, [t, liveGeoData, liveGeoData.latitude, lastKnownLocation, userPrefs])
-
-
+*/
 
   return (
     <StyledAppArea>
       <Card></Card>
       <Card title="Location Status">Location Status{txt}</Card>
+      <button type="button" disabled={ i18n.language === "es"}
+      onClick={() => changeLanguage("es")}>
+        {t("translation:es")}
+      </button>
+      <button type="button" disabled={ i18n.language === "en"}
+      onClick={() => changeLanguage("en")}>
+        {t("translation:en")}
+      </button>
+      <h1>{t("alert:hello", "wtf")}</h1>
+      {/* <p>{t('alert:content.text', 'Welcome here.')}</p> */}
     </StyledAppArea>
-  )
+  );
 }
 
-export default AppArea
+export default AppArea;
--- src/i18n.js
+++ src/i18n.js
@@ -11,7 +11,7 @@ i18n
     debug: true,
     lng: 'en',
     fallbackLng: 'en',
-    whitelist: ['en', 'de'],
+    whitelist: ['en', 'es'],
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
