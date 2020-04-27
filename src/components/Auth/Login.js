f87cbdaeceee04ac70a0b846ef5836d39f7d3137
Afshin Mokhtari <afshinator@hotmail.com>
2020-04-27 13:10:56 -0700

    Style login a bit

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,32 +1,36 @@
 import React from "react";
+import Card from './../Card';
 
 function Login(props) {
   const [login, setLogin] = React.useState(true);
 
   return (
-    <div>
-      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form className="flex flex-column">
+    <Card title="Login/Create Account" >
+      <h2 className="">{login ? "Login" : "Create Account"}</h2>
+      <form className="">
         {!login && (
           <input type="text" placeholder="Your name" autoComplete="off" />
         )}
         <input type="email" placeholder="Your email" autoComplete="off" />
+        <br />
         <input type="password" placeholder="Choose a secure password" />
-        <div className="flex mt3">
+        <div className="flex ">
           <button type="submit" className="button pointer mr2">
             Submit
           </button>
+          <br />
           <button
             type="button"
             className="pointer button"
             onClick={() => setLogin(prevLogin => !prevLogin)}
           >
-            {login ? "need to create an account?" : "already have an account?"}
+            {login ? "Need to create an account?" : "Already have an account?"}
           </button>
         </div>
       </form>
-    </div>
-  );
+    </Card>
+  )
+
 }
 
 export default Login;
