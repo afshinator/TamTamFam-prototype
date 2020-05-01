6a8e1046ab3b831cbbf2b3837aa871804ef44a76
Afshin Mokhtari <afshinator@hotmail.com>
2020-04-30 17:08:14 -0700

    Start adding FIREBASE

---
+++ src/firebase/firebase.js
@@ -0,0 +1,7 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './config';
+
+// Initialize Firebase
+app.initializeApp(firebaseConfig);
+// firebase.analytics();
