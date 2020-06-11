76b8037cb13c8d6a19a8692eb3dc35c351971daa
Afshin Mokhtari <afshinator@hotmail.com>
2020-06-11 13:48:32 -0700

    update dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,33 +1,21 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import { FirebaseContext } from "../firebase"
 import { useSpring, animated } from "react-spring"
-import { Player, Boot, FileProvider } from "@vime-js/complete"
+
 
 import Navi from "../components/Navi"
 
 export default function Landing() {
   const { user } = React.useContext(FirebaseContext)
   const { t } = useTranslation(["landing"])
-
+  const [s, setS] = useState(null)
+  
   useEffect(() => {
-    const target = document.getElementById("playerTarget")
-    const player = new Player({
-      target,
-      // If you'd like to initialize any props on setup, you can do so here.
-      props: {
-        src: "/src/assets/music.mp3",
-        plugins: [Boot],
-        providers: [FileProvider],
-      },
-    })
-    const off = player.$on("mount", () => {
-      // Interact with the player and plugins here.
-    })
+
     return () => {
-      off()
-      player.$destroy()
+
     }
   }, [])
 
