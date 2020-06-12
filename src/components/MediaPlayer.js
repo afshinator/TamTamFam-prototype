42218061639ece6dc706de3be627c78caf39cb17
Afshin Mokhtari <afshinator@hotmail.com>
2020-06-11 22:05:10 -0700

    Put in media player and make it draggable

---
+++ src/components/MediaPlayer.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+const MediaPlayer = ({
+  mediaType,
+  active,
+  autoplay,
+  endCallback,
+  progressCallback,
+}) => {
+  return (
+    <>
+      <section style={{ position: "relative" }}>
+        <ReactPlayer
+          // width="100%"
+          // height="100%"
+          // style={{ position: "absolute", top: "0", left: "0" }}
+          // playing={autoplay}
+          // controls={true}
+          url={active[mediaType]}
+          // onEnded={endCallback}
+          // onProgress={progressCallback}
+        />
+      </section>
+    </>
+  )
+}
+
+export default MediaPlayer
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,26 +3,51 @@ import { Link } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import { FirebaseContext } from "../firebase"
 import { useSpring, animated } from "react-spring"
-
-
+import ReactPlayer from "react-player"
+import Draggable from "react-draggable"
+import Emoji from 'a11y-react-emoji'
+import styled, { css } from 'styled-components'
 import Navi from "../components/Navi"
+import MediaPlayer from "../components/MediaPlayer"
+
+const StyledEmoji = styled(({ isSpinning, ...props }) => <Emoji {...props} />)`
+    font-size: 32px;
+
+    ${props => props.isSpinning && css`
+        animation: spinning 1s linear infinite;
+    `}
+`
 
 export default function Landing() {
   const { user } = React.useContext(FirebaseContext)
   const { t } = useTranslation(["landing"])
   const [s, setS] = useState(null)
-  
+
   useEffect(() => {
-
-    return () => {
-
-    }
+    return () => {}
   }, [])
 
+  const data = { video: "https://www.youtube.com/watch?v=UahWEszdezo" }
   return (
     <>
       <Navi />
       <div id="playerTarget" className="w-1/2 h-48"></div>
+      <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+      >
+        <div>
+          <div className="handle">
+
+          <StyledEmoji isSpinning={true} symbol="✋🏼" label="move" />
+          </div>
+          <MediaPlayer mediaType="video" active={data} />
+        </div>
+      </Draggable>
     </>
   )
 }
